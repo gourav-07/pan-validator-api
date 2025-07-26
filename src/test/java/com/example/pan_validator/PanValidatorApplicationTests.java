0b6ac56a32b1626f76d1d8441bac54d5b3361928
Gourav Prajapati <gouravprajapati@Gouravs-MacBook-Air.local>
2025-07-27 01:10:18 +0530

    Initial commit

---
+++ src/test/java/com/example/pan_validator/PanValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pan_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
